95ed0862b7331b4c72f1e6a6c3f33568b19f89d4
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-28 09:07:25 +0200

    API allows extensions to log their messages to a single file.

--- OverCR.ExtensionSystem.API/Debugging/Log.cs
+++ OverCR.ExtensionSystem.API/Debugging/Log.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace OverCR.ExtensionSystem.API.Debugging
 {
     public static class Log
     {
-        public static void WriteLine(string message)
+        public static void WriteLine(object caller, string message)
         {
-            
+            var callerName = caller.GetType().FullName;
+            string msg = $"{callerName} {DateTime.Now} -> {message}";
+
+            using (var sw = new StreamWriter("extension_log.txt"))
+            {
+                sw.WriteLine(msg);
+            }
         }
     }
 }
