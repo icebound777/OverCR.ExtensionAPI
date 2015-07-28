696d58c9d8bd83ce0e92535c1302c23cad7c312e
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-28 11:46:18 +0200

    Extensions can now load and save their settings.

---
+++ OverCR.ExtensionSystem.API/Configuration/Loader.cs
@@ -0,0 +1,13 @@
+﻿namespace OverCR.ExtensionSystem.API.Configuration
+{
+    public static class Loader
+    {
+        public static Settings RetrieveSettings(object caller)
+        {
+            if(Settings.Exist(caller))
+                return new Settings(caller);
+
+            return null;
+        }
+    }
+}
---
+++ OverCR.ExtensionSystem.API/Configuration/Settings.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using static OverCR.ExtensionSystem.API.Filesystem.Paths;
+
+namespace OverCR.ExtensionSystem.API.Configuration
+{
+    public class Settings : Dictionary<string, string>
+    {
+        private object _owner;
+
+        public Settings() { }
+
+        public Settings(object owner)
+        {
+            _owner = owner;
+            Initialize();      
+        }
+
+        public void Save()
+        { 
+            using (var sw = new StreamWriter($"{ExtensionSettingsDirectory}/{GetFileName()}", false))
+            {
+                foreach (var pair in this)
+                {
+                    sw.WriteLine($"{pair.Key}={pair.Value}");
+                }
+            }
+        }
+
+        public static Settings LoadFrom(string filePath)
+        {
+            using (var sr = new StreamReader(filePath))
+            {
+                return Parse(sr.ReadToEnd());
+            }
+        }
+
+        public static Settings Parse(string source)
+        {
+            var settings = new Settings();
+
+            foreach(var str in source.Split('\n'))
+            {
+                var trimmed = str.Trim();
+
+                if (trimmed.IndexOf('=') == -1)
+                    continue;
+
+                var statement = trimmed.Split('=');
+
+                var key = statement[0];
+                statement.RemoveAt(0);
+
+                var value = string.Join("", statement);
+
+                settings.Add(key, value);
+            }
+
+            return settings;
+        }
+
+        public static bool Exist(object caller)
+        {
+            return File.Exists($"{ExtensionSettingsDirectory}/{GetFileName(caller)}");
+        }
+
+        private string GetFileName()
+        {
+            return $"{_owner.GetType().Namespace}.cfg";
+        }
+
+        private static string GetFileName(object caller)
+        {
+            return $"{caller.GetType().Namespace}.cfg";
+        }
+
+        private void Initialize()
+        {
+            var settings = LoadFrom($"{ExtensionSettingsDirectory}/{GetFileName()}");
+            foreach (var pair in settings)
+            {
+                Add(pair.Key, pair.Value);
+            }
+            settings = null;
+        }
+    }
+}
