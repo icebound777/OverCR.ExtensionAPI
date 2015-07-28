885a153d650da6510f1b33ad1d99eb79a38ab0b6
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-28 09:29:51 +0200

    Moved Extension System's directories into the API project

---
+++ OverCR.ExtensionSystem.API/Filesystem/Paths.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace OverCR.ExtensionSystem.API.Filesystem
+{
+    public static class Paths
+    {
+        public static string ExtensionSystemRoot { get; } = $"{Application.dataPath}/ExtensionSystem";
+        public static string ExtensionDirectory { get; } = $"{ExtensionSystemRoot}/Plug-ins";
+        public static string ExtensionSettingsDirectory { get; } = $"{ExtensionSystemRoot}/Settings";
+    }
+}
--- OverCR.ExtensionSystem.Manager/Filesystem/Initializer.cs
+++ OverCR.ExtensionSystem.Manager/Filesystem/Initializer.cs
@@ -1,14 +1,10 @@
-﻿using UnityEngine;
+﻿using static OverCR.ExtensionSystem.API.Filesystem.Paths;
 using static System.IO.Directory;
 
 namespace OverCR.ExtensionSystem.Manager.Filesystem
 {
     internal static class Initializer
     {
-        internal static string ExtensionSystemRoot { get; } = $"{Application.dataPath}/ExtensionSystem";
-        internal static string ExtensionDirectory { get; } = $"{ExtensionSystemRoot}/Plug-ins";
-        internal static string ExtensionSettingsDirectory { get; } = $"{ExtensionSystemRoot}/Settings";
-
         internal static void InitializeExtensionFilesystem()
         {
             if(!Exists(ExtensionDirectory))
