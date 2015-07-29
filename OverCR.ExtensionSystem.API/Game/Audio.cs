a543fb9460c2c68f92c08cad82758716fef065ec
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-29 14:52:41 +0200

    Added audio class with basic custom music control.

---
+++ OverCR.ExtensionSystem.API/Game/Audio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OverCR.ExtensionSystem.API.Game
+{
+    public static class Audio
+    {
+        public static void EnableCustomMusic()
+        {
+            G.Sys.AudioManager_.EnableCustomMusic(true);
+        }
+
+        public static void DisableCustomMusic()
+        {
+            G.Sys.AudioManager_.EnableCustomMusic(false);
+        }
+    }
+}
