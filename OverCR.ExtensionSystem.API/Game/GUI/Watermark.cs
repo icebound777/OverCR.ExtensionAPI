08e80f3f3f83d8384627691920122b5dcf77b6f6
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-29 20:46:14 +0200

    Added a watermark text manipulation class.

---
+++ OverCR.ExtensionSystem.API/Game/GUI/Watermark.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace OverCR.ExtensionSystem.API.Game.GUI
+{
+    public static class Watermark
+    {
+        private static UILabel _watermarkComponent;
+
+        static Watermark()
+        {
+            _watermarkComponent = GameObject.Find("AlphaVersion").GetComponent<UILabel>();
+        }
+
+        public static string Text
+        {
+            get { return _watermarkComponent.text; }
+            set { _watermarkComponent.text = value; }
+        }
+    }
+}
