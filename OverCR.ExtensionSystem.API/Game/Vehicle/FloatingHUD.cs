e7c40c8ada9cef95c4b5ca0c2b2ce74c5a186484
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-30 08:06:25 +0200

    Added a possibility to manipulate the hover text near the vehicle.

---
+++ OverCR.ExtensionSystem.API/Game/Vehicle/FloatingHUD.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace OverCR.ExtensionSystem.API.Game.Vehicle
+{
+    public static class FloatingHUD
+    {
+        private static GameObject _localCarObject;
+        private static HoverScreenEmitter _hoverScreenEmitter;
+
+        static FloatingHUD()
+        {
+            DetectCarObject();
+        }
+
+        private static void DetectCarObject()
+        {
+            if(_localCarObject == null)
+                _localCarObject = GameObject.Find("LocalCar");
+
+            if (_hoverScreenEmitter == null)
+                _hoverScreenEmitter = _localCarObject?.GetComponent<HoverScreenEmitter>();
+        }
+
+        public static void ShowText(float time, int priority, string text, float alphaScale)
+        {
+            var trickText = new TrickyTextLogic.TrickText(time, priority, TrickyTextLogic.TrickText.TextType.standard, text, alphaScale);
+            _hoverScreenEmitter.SetTrickText(trickText);
+        }
+    }
+}
