d27515d19601c8e8164a9e0da75725ecdfaac29d
Tomasz Cichoń <mrciastexx@gmail.com>
2015-07-31 20:54:04 +0200

    Added a way to get versions for Distance and Extension System.

---
+++ OverCR.ExtensionSystem.API/Runtime/Version.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace OverCR.ExtensionSystem.API.Runtime
+{
+    public class Version
+    {
+        public static class Distance
+        {
+            public static int Build
+            {
+                get { return SVNRevision.number_; }
+            }
+        }
+
+        public static class ExtensionSystem
+        {
+            public static int Major
+            {
+                get { return Assembly.GetAssembly(typeof(Version)).GetName().Version.Major; }
+            }
+
+            public static int Minor
+            {
+                get { return Assembly.GetAssembly(typeof(Version)).GetName().Version.Minor; }
+            }
+        }
+    }
+}
