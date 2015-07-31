e6194176886ef9278be07030c22e9213d33aad00
Tomasz Cichoń <mrciastexx@gmail.com>
2015-08-01 01:07:45 +0200

    Added car screen manipulation functions.

---
+++ OverCR.ExtensionSystem.API/Game/Vehicle/Screen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace OverCR.ExtensionSystem.API.Game.Vehicle
+{
+    public static class Screen
+    {
+        private static GameObject _localScreenGroupObject;
+        private static CarScreenLogic _carScreenLogic;
+
+        static Screen()
+        {
+            DetectCarObject();
+        }
+
+        public static void WriteText(string text, float speed = 1.0f, float delay = 0.0f, bool clearOnFinish = true, string timeBarText = "")
+        {
+            _carScreenLogic.DecodeText(text, speed, delay, clearOnFinish, timeBarText);
+        }
+
+        public static void Clear()
+        {
+            _carScreenLogic.ClearDecodeText();
+        }
+
+        private static void DetectCarObject()
+        {
+            if (_localScreenGroupObject == null)
+                _localScreenGroupObject = GameObject.Find("CarScreenGroup");
+
+            if (_carScreenLogic == null)
+                _carScreenLogic = _localScreenGroupObject?.GetComponent<CarScreenLogic>();
+        }
+    }
+}
