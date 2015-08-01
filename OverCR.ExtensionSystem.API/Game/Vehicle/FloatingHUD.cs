b4971c1791cf34ca2f85c5800f727367d22d7aa7
Tomasz Cichoń <mrciastexx@gmail.com>
2015-08-01 09:57:36 +0200

    Renamed ShowText method to WriteText.

--- OverCR.ExtensionSystem.API/Game/Vehicle/FloatingHUD.cs
+++ OverCR.ExtensionSystem.API/Game/Vehicle/FloatingHUD.cs
@@ -21,10 +21,15 @@ namespace OverCR.ExtensionSystem.API.Game.Vehicle
                 _hoverScreenEmitter = _localCarObject?.GetComponent<HoverScreenEmitter>();
         }
 
-        public static void ShowText(float time, int priority, string text, float alphaScale)
+        public static void WriteText(float time, int priority, string text, float alphaScale)
         {
             var trickText = new TrickyTextLogic.TrickText(time, priority, TrickyTextLogic.TrickText.TextType.standard, text, alphaScale);
             _hoverScreenEmitter.SetTrickText(trickText);
+        }
+
+        public static void Show()
+        {
+
         }
     }
 }
