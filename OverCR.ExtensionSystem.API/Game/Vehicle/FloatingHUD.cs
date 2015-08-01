45ea3b16c7a9fb9c416f91b41fb305f1c3e08b3a
Tomasz Cichoń <mrciastexx@gmail.com>
2015-08-01 09:58:57 +0200

    Made it so car object is always detected.

--- OverCR.ExtensionSystem.API/Game/Vehicle/FloatingHUD.cs
+++ OverCR.ExtensionSystem.API/Game/Vehicle/FloatingHUD.cs
@@ -23,13 +23,15 @@ namespace OverCR.ExtensionSystem.API.Game.Vehicle
 
         public static void WriteText(float time, int priority, string text, float alphaScale)
         {
+            DetectCarObject();
+
             var trickText = new TrickyTextLogic.TrickText(time, priority, TrickyTextLogic.TrickText.TextType.standard, text, alphaScale);
             _hoverScreenEmitter.SetTrickText(trickText);
         }
 
         public static void Show()
         {
-
+            
         }
     }
 }
