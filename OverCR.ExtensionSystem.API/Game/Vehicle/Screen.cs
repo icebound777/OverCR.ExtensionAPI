6147479c1a17f266396e6d874090ef50002421e7
Tomasz Cichoń <mrciastexx@gmail.com>
2015-08-01 09:07:34 +0200

    Made it so text is wrapped on screen.

--- OverCR.ExtensionSystem.API/Game/Vehicle/Screen.cs
+++ OverCR.ExtensionSystem.API/Game/Vehicle/Screen.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace OverCR.ExtensionSystem.API.Game.Vehicle
 {
     public static class Screen
     {
+        public static int ScreenColumns { get; } = 20;
+
         private static GameObject _localScreenGroupObject;
         private static CarScreenLogic _carScreenLogic;
 
@@ -18,11 +17,16 @@ namespace OverCR.ExtensionSystem.API.Game.Vehicle
 
         public static void WriteText(string text, float speed = 1.0f, float delay = 0.0f, bool clearOnFinish = true, string timeBarText = "")
         {
-            _carScreenLogic.DecodeText(text, speed, delay, clearOnFinish, timeBarText);
+            DetectCarObject();
+
+            var wrappedForScreen = Regex.Replace(text, $"\\w.{{{ScreenColumns}}}", "$0\n");
+            _carScreenLogic.DecodeText(wrappedForScreen, speed, delay, clearOnFinish, timeBarText);
         }
 
         public static void Clear()
         {
+            DetectCarObject();
+
             _carScreenLogic.ClearDecodeText();
         }
 
