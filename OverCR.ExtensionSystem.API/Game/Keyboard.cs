08c58c1dc23fd05e016b043218a86ce954be12db
Tomasz Cichoń <mrciastexx@gmail.com>
2015-08-01 09:33:37 +0200

    Added a way to get keyboard inputs with raw string.

---
+++ OverCR.ExtensionSystem.API/Game/Keyboard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace OverCR.ExtensionSystem.API.Game
+{
+    public static class Keyboard
+    {
+        public static bool GetKeyDown(string key)
+        {
+            return Input.GetKeyDown(
+                ParseKeyCode(key)
+            );
+        }
+
+        public static bool GetKeyUp(string key)
+        {
+            return Input.GetKeyUp(
+                ParseKeyCode(key)
+            );
+        }
+
+        public static bool GetKey(string key)
+        {
+            return Input.GetKey(
+                ParseKeyCode(key)
+            );
+        }
+
+        private static KeyCode ParseKeyCode(string mnemonic)
+        {
+            return (KeyCode)Enum.Parse(typeof(KeyCode), mnemonic);
+        }
+    }
+}
