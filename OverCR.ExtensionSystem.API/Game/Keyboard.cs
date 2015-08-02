aa181295810060943edf5c667c703b79d6d52f1a
Tomasz Cichoń <mrciastexx@gmail.com>
2015-08-02 12:23:11 +0200

    Made keystroke detection less error-prone.

--- OverCR.ExtensionSystem.API/Game/Keyboard.cs
+++ OverCR.ExtensionSystem.API/Game/Keyboard.cs
@@ -7,23 +7,54 @@ namespace OverCR.ExtensionSystem.API.Game
     {
         public static bool GetKeyDown(string key)
         {
-            return Input.GetKeyDown(
-                ParseKeyCode(key)
-            );
+            if (string.IsNullOrEmpty(key))
+                return false;
+
+            try
+            {
+                return Input.GetKeyDown(
+                    ParseKeyCode(key)
+                );
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public static bool GetKeyUp(string key)
         {
-            return Input.GetKeyUp(
-                ParseKeyCode(key)
-            );
+            if (string.IsNullOrEmpty(key))
+                return false;
+
+            try
+            {
+                return Input.GetKeyUp(
+                    ParseKeyCode(key)
+                );
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public static bool GetKey(string key)
         {
-            return Input.GetKey(
-                ParseKeyCode(key)
-            );
+            if (string.IsNullOrEmpty(key))
+                return false;
+
+            try
+            {
+                return Input.GetKey(
+                    ParseKeyCode(key)
+                );
+            }
+            catch
+            {
+                return false;
+            }
+
         }
 
         private static KeyCode ParseKeyCode(string mnemonic)
