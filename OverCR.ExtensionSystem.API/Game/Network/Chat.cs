2d5c53dcfbd9c47a66db0d508cedfe3a70e7df0b
Tomasz Cichoń <mrciastexx@gmail.com>
2015-08-02 12:58:44 +0200

    Fixed an issue where local user would receive own message.

--- OverCR.ExtensionSystem.API/Game/Network/Chat.cs
+++ OverCR.ExtensionSystem.API/Game/Network/Chat.cs
@@ -8,22 +8,32 @@ namespace OverCR.ExtensionSystem.API.Game.Network
         public static event MessageHandler MessageReceived;
         public static event MessageHandler MessageSent;
 
+        private static bool justSent;
+
         static Chat()
         {
-            // Events.Local.Message.Subscribe(OnMessageReceived);
             Events.Local.ChatSubmitMessage.Subscribe(OnMessageSent);
             Events.ClientToAllClients.ChatMessage.Subscribe(OnMessageReceived);
         }
 
         private static void OnMessageReceived(Events.ClientToAllClients.ChatMessage.Data data)
         {
+            if(justSent)
+            {
+                justSent = false;
+                return;
+            }
             var extracted = ExtractData(data.message_);
-            if(extracted != null)
-                MessageReceived?.Invoke(extracted[0], extracted[1]);
+
+            MessageReceived?.Invoke(extracted?[0], extracted?[1]);
         }
 
         private static void OnMessageSent(Events.Local.ChatSubmitMessage.Data data)
         {
+            if (string.IsNullOrEmpty(data.message_))
+                return;
+
+            justSent = true;
             MessageSent?.Invoke(G.Sys.ProfileManager_.CurrentProfile_.Name_, data.message_);
         }
 
@@ -39,12 +49,19 @@ namespace OverCR.ExtensionSystem.API.Game.Network
             user = user.Remove(0, 8);
 
             if (user.LastIndexOf('[') == -1)
+            {
                 return null;
+            }
 
             user = user.Remove(user.LastIndexOf('['), 8);
 
             splitData = splitData.RemoveAt(0);
             var messageString = string.Join("", splitData);
+
+            if(string.IsNullOrEmpty(messageString))
+            {
+                return null;
+            }
 
             return new[] { user, messageString };
         }
