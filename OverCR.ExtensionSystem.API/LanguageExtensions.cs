3ddea6e51edae307832a96b85d8ef48e366b798a
Tomasz Cichoń <mrciastexx@gmail.com>
2015-08-02 12:23:35 +0200

    Word wrapping is now more reliable.

--- OverCR.ExtensionSystem.API/LanguageExtensions.cs
+++ OverCR.ExtensionSystem.API/LanguageExtensions.cs
@@ -1,18 +1,62 @@
 ﻿using System;
-using System.Linq;
+using System.Text;
 
 namespace OverCR.ExtensionSystem.API
 {
     internal static class LanguageExtensions
     {
-        internal static string WordBreak(this string text, int lineLength)
+        internal static string WordWrap(this string text, int lineLength)
         {
-            var charCount = 0;
-            var lines = text.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
-                            .GroupBy(w => (charCount += w.Length + 1) / lineLength)
-                            .Select(x => string.Join(" ", x.ToArray()));
+            int position;
+            int next;
+            var sb = new StringBuilder();
 
-            return string.Join("\n", lines.ToArray());
+            if (lineLength < 1)
+                return text;
+
+            for (position = 0; position < text.Length; position = next)
+            {
+                int lineEnd = text.IndexOf('\n', position);
+                if (lineEnd == -1)
+                    next = lineEnd = text.Length;
+                else
+                    next = lineEnd + 1;
+
+                if (lineEnd > position)
+                {
+                    do
+                    {
+                        int length = lineEnd - position;
+
+                        if (length > lineLength)
+                            length = LineBreak(text, position, lineLength);
+
+                        sb.Append(text, position, length);
+                        sb.Append('\n');
+
+                        position += length;
+                        while (position < lineEnd && char.IsWhiteSpace(text[position]))
+                            position++;
+                    } while (lineEnd > position);
+                }
+                else sb.Append('\n');
+            }
+            return sb.ToString();
+        }
+
+        private static int LineBreak(string text, int where, int max)
+        {
+            int i = max;
+            while (i >= 0 && !char.IsWhiteSpace(text[where + i]))
+                i--;
+
+            if (i < 0)
+                return max;
+
+            while (i >= 0 && char.IsWhiteSpace(text[where + i]))
+                i--;
+
+            return i + 1;
         }
     }
 }
