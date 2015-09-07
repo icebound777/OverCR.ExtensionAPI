fd3cf2110f5e1f76b2bc5c76d77357b3764987e6
Tomasz Cichoń <mrciastexx@gmail.com>
2015-09-07 10:08:55 +0200

    Migrated class to C# 6.0.

--- OverCR.ExtensionSystem.API/Game/Player/Profile.cs
+++ OverCR.ExtensionSystem.API/Game/Player/Profile.cs
@@ -2,35 +2,17 @@ namespace OverCR.ExtensionSystem.API.Game.Player
 {
     public static class Profile
     {
-        public static string Name
-        {
-            get { return G.Sys.ProfileManager_.CurrentProfile_.Name_; }
-        }
+        public static string Name => G.Sys.ProfileManager_.CurrentProfile_.Name_;
 
-        public static string ActiveVehicleName
-        {
-            get { return G.Sys.ProfileManager_.CurrentProfile_.CarName_; }
-        }
+        public static string ActiveVehicleName => G.Sys.ProfileManager_.CurrentProfile_.CarName_;
 
-        public static string PrimaryColor
-        {
-            get { return G.Sys.ProfileManager_.CurrentProfile_.Primary_.ToHexStringRGB(); }
-        }
+        public static string PrimaryColor => G.Sys.ProfileManager_.CurrentProfile_.Primary_.ToHexStringRGB();
 
-        public static string SecondaryColor
-        {
-            get { return G.Sys.ProfileManager_.CurrentProfile_.Secondary_.ToHexStringRGB(); }
-        }
+        public static string SecondaryColor => G.Sys.ProfileManager_.CurrentProfile_.Secondary_.ToHexStringRGB();
 
-        public static string GlowColor
-        {
-            get { return G.Sys.ProfileManager_.CurrentProfile_.Glow_.ToHexStringRGB(); }
-        }
+        public static string GlowColor => G.Sys.ProfileManager_.CurrentProfile_.Glow_.ToHexStringRGB();
 
-        public static string SparkleColor
-        {
-            get { return G.Sys.ProfileManager_.CurrentProfile_.Sparkle_.ToHexStringRGB(); }
-        }
+        public static string SparkleColor => G.Sys.ProfileManager_.CurrentProfile_.Sparkle_.ToHexStringRGB();
 
         public static float SteeringSensitivity
         {
