519e0d030628b1fcb65d466fff0333c413db5256
Tomasz Cichoń <mrciastexx@gmail.com>
2015-09-08 14:22:44 +0200

    Added text alignment change option.

--- OverCR.ExtensionSystem.API/Game/GUI/Dialog.cs
+++ OverCR.ExtensionSystem.API/Game/GUI/Dialog.cs
@@ -2,24 +2,63 @@ namespace OverCR.ExtensionSystem.API.Game.GUI
 {
     public static class Dialog
     {
+        public enum TextAlignment
+        {
+            Left,
+            Center,
+            Right
+        }
+
         public delegate void MessageDialogDelegate();
 
-        public static void ShowMessageDialog(string title, string message, MessageDialogDelegate actionAfter = null)
+        public static void ShowMessageDialog(string title, string message, TextAlignment textAlignment = TextAlignment.Center, MessageDialogDelegate actionAfter = null)
         {
+            var pivot = UIWidget.Pivot.Center;
+
+            switch (textAlignment)
+            {
+                case TextAlignment.Left:
+                    pivot = UIWidget.Pivot.Left;
+                    break;
+                case TextAlignment.Center:
+                    pivot = UIWidget.Pivot.Center;
+                    break;
+                case TextAlignment.Right:
+                    pivot = UIWidget.Pivot.Right;
+                    break;
+            }
+
             G.Sys.MenuPanelManager_?.ShowError(
                 message,
                 title,
-                () => actionAfter?.Invoke()
+                () => actionAfter?.Invoke(),
+                pivot
             );
         }
 
-        public static void ShowDecisionDialog(string title, string message, MessageDialogDelegate okAction = null, MessageDialogDelegate cancelAction = null)
+        public static void ShowDecisionDialog(string title, string message, TextAlignment textAlignment = TextAlignment.Center, MessageDialogDelegate okAction = null, MessageDialogDelegate cancelAction = null)
         {
+            var pivot = UIWidget.Pivot.Center;
+
+            switch (textAlignment)
+            {
+                case TextAlignment.Left:
+                    pivot = UIWidget.Pivot.Left;
+                    break;
+                case TextAlignment.Center:
+                    pivot = UIWidget.Pivot.Center;
+                    break;
+                case TextAlignment.Right:
+                    pivot = UIWidget.Pivot.Right; 
+                    break;
+            }
+
             G.Sys.MenuPanelManager_?.ShowOkCancel(
                 message,
                 title,
                 () => okAction?.Invoke(),
-                () => cancelAction?.Invoke()
+                () => cancelAction?.Invoke(),
+                pivot
             );
         }
     }
